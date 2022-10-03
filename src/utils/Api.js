fcba4865727c70b20f88f62e62db144b2eb60fec
Aziz <t1-zer@yandex.ru>
2022-10-03 11:05:16 +0300

    feat: update api

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 function Api(lang, text) {
   return fetch(
-    `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=&lang=${lang}&text=${text}`
+    `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20220929T092800Z.5954d8a03f2e8536.536b1e6b42797705cc8597002055dc25e3ee81a7&lang=${lang}&text=${text}`
   ).then((res) => {
     if (res.ok) {
       return res.json();
